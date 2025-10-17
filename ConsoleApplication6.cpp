9f21c42c5ed7d71b87abc29188adaf950c0853a5
Сектор_пудж <134601371+testpeopl@users.noreply.github.com>
2025-10-17 09:15:45 +0300

    практическая работа 1

---
+++ ConsoleApplication6.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	int alpha = 13;
+	cout << "alpha = " << alpha << " | тип: int, размер: 4 б." << endl;
+	int alpha_2 = 12;
+	cout << "alpha_2 = " << alpha_2 << " | тип: int, размер: 4 б." << endl;
+	int alpha_3 = 11;
+	cout << "alpha_3 = " << alpha_3 << " | тип: int, размер: 4 б." << endl;
+	int alpha_4 = 14;
+	cout << "alpha_4 = " << alpha_4 << " | тип: int, размер: 4 б." << endl;
+	int alpha_5 = 15;
+	cout << "alpha_5 = " << alpha_5 << " | тип: int, размер: 4 б." << endl;
+	short delta = 3;
+	cout << "delta = " << delta << " | тип: short, размер: 2 б." << endl;
+	short delta_2 = 8;
+	cout << "delta_2 = " << delta_2 << " | тип: short, размер: 2 б." << endl;
+	short delta_3 = 7;
+	cout << "delta_3 = " << delta_3 << " | тип: short, размер: 2 б." << endl;
+	short delta_4 = 6;
+	cout << "delta_4 = " << delta_4 << " | тип: short, размер: 2 б." << endl;
+	short delta_5 = 1;
+	cout << "delta_5 = " << delta_5 << " | тип: short, размер: 2 б." << endl;
+	float grey = 1.4;
+	cout << "grey = " << grey << " | тип: float, размер: 4 б." << endl;
+	float grey2 = 1.9;
+	cout << "grey2 = " << grey2 << " | тип: float, размер: 4 б." << endl;
+	float grey3 = 1.1;
+	cout << "grey3 = " << grey3 << " | тип: float, размер: 4 б." << endl;
+	float grey4 = 1.2;
+	cout << "grey4 = " << grey4 << " | тип: float, размер: 4 б." << endl;
+	float grey5 = 1.6;
+	cout << "grey5 = " << grey5 << " | тип: float, размер: 4 б." << endl;
+	double red = 1.47;
+	cout << "red = " << red << " | тип: double, размер: 8 б." << endl;
+	double red2 = 1.37;
+	cout << "red2 = " << red2 << " | тип: double, размер: 8 б." << endl;
+	double red3 = 1.17;
+	cout << "red3 = " << red3 << " | тип: double, размер: 8 б." << endl;
+	double red4 = 1.56;
+	cout << "red4 = " << red4 << " | тип: double, размер: 8 б." << endl;
+	double red5 = 1.87;
+	cout << "red5 = " << red5 << " | тип: double, размер: 8 б." << endl;
+	char krey = 'a';
+	cout << "krey = " << krey << " | тип: char, размер: 1 б." << endl;
+	char krey2 = 'b';
+	cout << "krey2 = " << krey2 << " | тип: char, размер: 1 б." << endl;
+	char krey3 = 'c';
+	cout << "krey3 = " << krey3 << " | тип: char, размер: 1 б." << endl;
+	char krey4 = 'd';
+	cout << "krey4 = " << krey4 << " | тип: char, размер: 1 б." << endl;
+	char krey5 = 'e';
+	cout << "krey5 = " << krey5 << " | тип: char, размер: 1 б." << endl;
+
+}
