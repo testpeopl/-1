05097ba7013a976ccf5628449f69e06cb24a5779
Сектор_пудж <134601371+testpeopl@users.noreply.github.com>
2025-10-17 09:58:26 +0300

    практическая работа 2

---
+++ времена года.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	int a;
+	cout << "введите число:";
+	cin >> a;
+	if (a == 1) {
+		cout << "Лето, тепло и жарко" << endl;
+	}
+	else if (a == 2) {
+		cout << "Сейчас осень, следует одеться тепло и взять с собой зонт" << endl;
+	}
+	else if (a == 3) {
+		cout << "зима, сскоро новвй год" << endl;
+	}
+	else if (a == 4) {
+		cout << "веста, скоро лето" << endl;
+	}
+	else {
+		cout << "такого времени года нет" << endl;
+	}
+	return 0;
+}
