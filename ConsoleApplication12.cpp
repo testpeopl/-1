ddb75a2b48f16633cb3aaf3d3bb5ee8dde019672
Сектор_пудж <134601371+testpeopl@users.noreply.github.com>
2025-10-23 17:01:47 +0300

    пз5

---
+++ ConsoleApplication12.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    char operation;
+    double num1, num2, result;
+    cout << "калькулятор";
+    cout << "введите число 1: ";
+    cin >> num1;
+    cout << "введите число 2: ";
+    cin >> num2;
+    if (operation == '+') {
+        result = num1 + num2;
+        cout << result << endl;
+    }
+    else if (operation == '-') {
+        result = num1 - num2;
+        cout << result << endl;
+    }
+    else if (operation == '*') {
+        result = num1 * num2;
+        cout << result << endl;
+    }
+    else if (operation == '/'){
+        if (num2 !=0)
+        result = num1 / num2;
+        cout << 
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
