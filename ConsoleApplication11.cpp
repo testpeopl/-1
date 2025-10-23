8bc6fa2593a143170b4528ff9a0223283ff20da1
Сектор_пудж <134601371+testpeopl@users.noreply.github.com>
2025-10-23 16:29:30 +0300

    Add files via upload

---
+++ ConsoleApplication11.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num = 0;
+    cout << "9 * 1" << endl;
+    cout << "введите число: ";
+    cin >> num;
+    if (num == 9) {
+        cout << "все верно";
+    }
+    else
+        cout << "ошибка";
+
+    
+    int num2 = 0;
+    cout << "9 * 2" << endl;
+    cout << "введите число: ";
+    cin >> num;
+    if (num2 == 18) {
+        cout << " Примеры решены, поздравляем! ";
+    }
+    else
+        cout << "Ошибка, пример решен неверно!";
+}
