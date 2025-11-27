bf46ba09e90f7a8229102bf346c69eb3aadf3469
Сектор_пудж <134601371+testpeopl@users.noreply.github.com>
2025-11-27 15:28:19 +0300

    самостоятельная работа 9

---
+++ ConsoleApplication32.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	int a;
+	cout << "Ведите число:";
+	cin >> a;
+	if (a == 1) {
+		cout << "Лето" << endl;
+	}
+	else if (a == 2) {
+		cout << "Зима" << endl;
+	}
+	else if (a == 3) {
+		cout << "Весна" << endl;
+	}
+	else if (a == 4) {
+		cout << "Осень" << endl;
+	}
+	else {
+		cout << "Ошибка" << endl;
+	}
+	return 0;
+}
