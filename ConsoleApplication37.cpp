4822b104d1604f47b452a2f0ae6bb7e3d099fe5f
Сектор_пудж <134601371+testpeopl@users.noreply.github.com>
2025-12-04 14:17:40 +0300

    практическая W1

---
+++ ConsoleApplication37.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	setlocale(0, "");
+	int num;
+	int sum = 0;
+	cout << "напиши число: ";
+	cin >> num;
+	while (num != 0) {
+		sum += num;
+			cout << "введите ещё число: ";
+			cin >> num;
+	}
+	cout << "сумма: " << sum << endl;
+		return 0;
+}
