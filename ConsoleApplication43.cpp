2dfd6dcce1eb4a2a909c2e978772e692133073f9
Сектор_пудж <134601371+testpeopl@users.noreply.github.com>
2025-12-05 12:38:54 +0300

    Практическая W5

---
+++ ConsoleApplication43.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    setlocale(0, "");
+    int i;
+    int sum;
+    int num = 1;
+    cout << "Введите число: ";
+    cin >> i;
+    while (num < 11) {
+        sum = i * num;
+        cout << i << "*" << num << "=" << sum << endl;
+        num++;
+    }
+    return 0;
+}
+
