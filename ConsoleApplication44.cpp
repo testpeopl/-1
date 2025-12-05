04fadd567a37a4e8524d44c7269dbee3a6b1688d
Сектор_пудж <134601371+testpeopl@users.noreply.github.com>
2025-12-05 12:44:19 +0300

    Практическая W6

---
+++ ConsoleApplication44.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	setlocale(0, "");
+	int num;
+	int sum = 0;
+	int u = 0;
+	int arif;
+	cout << "напиши число: ";
+	cin >> num;
+	while (num != 0) {
+		sum += num;
+		cout << "введите ещё число: ";
+		cin >> num;
+		u++;
+		arif = sum / u;
+	}
+	cout << "Среднее арифметическое	 " << arif << endl;
+	
+	return 0;
+}
