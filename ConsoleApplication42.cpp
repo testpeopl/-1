2e0a7423b22af65e5b797c574d5d420d119a7615
Сектор_пудж <134601371+testpeopl@users.noreply.github.com>
2025-12-05 12:24:09 +0300

    Практическая W4

---
+++ ConsoleApplication42.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	setlocale(0, "");
+int i;
+int b = 0;
+cout << "Введите число: " << endl;
+cin >> i;
+while (i != 0) {
+	i = i / 10;
+	b++;
+}
+cout << "Количество цифр:" << b << endl;
+return 0;
+}
