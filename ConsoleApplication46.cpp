57034616cfa192c327a5f7f6554973da5161430b
Сектор_пудж <134601371+testpeopl@users.noreply.github.com>
2025-12-11 13:49:38 +0300

    практическая W7

---
+++ ConsoleApplication46.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	char i[] = "12345";
+	char n[100];
+
+	while (strcmp(n, i) != 0) {
+		cout << "Введите пароль: " << endl;
+		cin >> n;
+		if (strcmp(n, i) != 0) 
+			cout << "неверно" << endl;
+	}
+	cout << "Добро пожаловать!" << endl;
+	return 0;
+}
