6b110923974f378f5563c4f138aae2559edf50b9
Сектор_пудж <134601371+testpeopl@users.noreply.github.com>
2025-12-12 11:21:22 +0300

    Практическая W2

---
+++ ConsoleApplication41.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(0, "");
+    int i;
+    cout << "Введите число: ";
+    cin >> i;
+    while (i > 0) {
+        cout << "Отсчет:" << i << endl;
+        i--;
+    }
+    cout << "Старт!";
+
+    return 0;
+}
+
