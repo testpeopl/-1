0710d7b0583ec6803fc70e7e83a8db4df8b72c84
Сектор_пудж <134601371+testpeopl@users.noreply.github.com>
2025-12-25 14:49:47 +0300

    ёлка

---
+++ ConsoleApplication54.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    cout << " \x1b[6m    * " << endl;
+    cout << " \x1b[32m    *" << endl;
+    cout << "\x1b[93m    *\x1b[32m*\x1b[91m*" << endl;
+    cout << " \x1b[98m  *\x1b[32m*\x1b[96m*" << "\x1b[32m**" << endl;
+    cout << " \x1b[32m **\x1b[31m*\x1b[32m***\x1b[95m*" << endl;
+    cout << " \x1b[32m*\x1b[96m*\x1b[32m***\x1b[93m*\x1b[32m***" << endl;
+    cout << " \x1b[90m    #     " << endl;
+    
+    int _; cin >> _;
+    return 0;
+
+}
+
