45d53c311ad06863b2b45f899a97ca2fe462f598
Сектор_пудж <134601371+testpeopl@users.noreply.github.com>
2026-01-30 12:43:41 +0300

    Ez

---
+++ ConsoleApplication68.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+
+    int ramzam;
+    char pididi;
+    cin >> ramzam;
+    cin >> pididi;
+    for (int i = 0; i < ramzam; i++) { //горизонталь
+        for (int j = 0; j < ramzam; j++) { //вертикаль
+            if (i == 0 || i == ramzam - 1 || j == 0 || j == ramzam - 1) {
+                cout << pididi << " ";
+            }
+            else if (j == ramzam /2 || i == ramzam /2){
+                cout << pididi << " ";
+            }
+            else if (j >= ramzam /2 && i >= ramzam /2){
+                cout <<  "\x1b[91m#\x1b[0m"<< " ";
+            }
+            else if (j <= ramzam / 2 && i >= ramzam / 2) {
+                cout << "\x1b[93m#\x1b[0m" << " ";
+            }
+            else if (j >= ramzam / 2 && i <= ramzam / 2) {
+                cout << "\x1b[92m#\x1b[0m" << " ";
+            }
+            else if (j <= ramzam / 2 && i <= ramzam / 2) {
+                cout << "\x1b[94m#\x1b[0m" << " ";
+            }
+            else {
+                cout << "  ";
+            }
+        }
+        cout << "\n";
+    }
+    return 0;
+}
